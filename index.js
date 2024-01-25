7d8a80032cb6a450f50a4e58a7022d23206e8dc4
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-25 20:05:35 +0600

    vercel json file added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express();
+var cors = require('cors')
 const Port = 5000;
 
+app.use(cors());
+const chefData = require('./Data/chef.json');
+
 app.get('/', (req, res)=>{
-    res.send('server side Init')
+    res.send(chefData)
+})
+app.get('/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedChef = chefData.find(singleChef => singleChef._id === id) || {};
+    res.send(selectedChef)
 })
 
 app.listen(Port, ()=>{
